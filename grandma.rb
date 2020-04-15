f2dad04425515d9322865ee89279629e8b66970a
Andrew Alteri <andrewsalteri@gmail.com>
2020-04-15 18:59:49 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,13 +1,11 @@
+
 def speak_to_grandma(phrase)
-    if phrase == "I LOVE YOU GRANDMA!"
-      return "I LOVE YOU TOO PUMPKIN!"
-    elsif phrase == "WHAT DID YOU EAT TODAY?"
-      return "NO, NOT SINCE 1938!"
-    elsif phrase == "WHAT?"
-      return "NO, NOT SINCE 1938!"
-    elsif phrase == "Hi Nana, how are you?"
-      return "HUH?! SPEAK UP, SONNY!"
-    else phrase == "Hi!"
-      return "HUH?! SPEAK UP, SONNY!"
-    end
+  if phrase == "I LOVE YOU GRANDMA!"
+     "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == phrase.upcase
+    "NO, NOT SINCE 1938!"
+  else
+    "HUH?! SPEAK UP, SONNY!"
+
   end
+end
