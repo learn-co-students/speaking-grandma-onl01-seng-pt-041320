6556ef3ec158b0e97522289802c460e1ab438397
Anmol Kaur <kaur.anmol26@gmail.com>
2020-04-17 20:09:25 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -18,7 +18,7 @@ def speak_to_grandma(phrase)
     "I LOVE YOU TOO PUMPKIN!"
   elsif phrase != phrase.upcase
     "HUH?! SPEAK UP, SONNY!"
-  else phrase == phrase.upcase
+  else 
     "NO, NOT SINCE 1938!"
   end
 end
